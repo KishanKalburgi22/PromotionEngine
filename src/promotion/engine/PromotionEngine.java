04263599ef7841cc8abbaf55840994752572cdea
Kishan_KK <kishankalburgi22@gmail.com>
2020-10-04 13:13:15 +0530

    Adapted code to handle the given scenarios

--- src/promotion/engine/PromotionEngine.java
+++ src/promotion/engine/PromotionEngine.java
@@ -1,6 +1,9 @@
 package promotion.engine;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,6 +11,7 @@ public class PromotionEngine {
 	public static int TotalAmount = 0;
 	private static Map<String, Integer> unitPrice = new HashMap<>();
 	private static Map<String, String> promotions = new HashMap<>();
+	private static List<String> items = new ArrayList<>();
 
 	private static void init() {
 		// Unit price SKU
@@ -31,22 +35,43 @@ public class PromotionEngine {
 		int total = sc.nextInt();
 
 		System.out.println("Enter the " + total + " input items in -> [item - quantity] format E.g 1-A");
+		String[] input = new String[total];
 		for (int i = 0; i < total; i++) {
-			calTotal(sc.next());
+			input[i] = sc.next();
+			String quantity = input[i].split("-")[0];
+			String item = input[i].split("-")[1];
+			for (int j = 0; j < Integer.parseInt(quantity); j++) {
+				items.add(item);
+			}
 		}
+		calTotal(items);
+
 		System.out.println("The Total Amount after discount is - " + TotalAmount);
+		items.clear();
+
 	}
 
-	public static int calTotal(String input) {
-		String quantity = input.split("-")[0];
-		String item = input.split("-")[1];
-
-		String q = promotions.get(item).split("-")[0];
-		String d = promotions.get(item).split("-")[2];
-		TotalAmount += (Integer.parseInt(quantity) / Integer.parseInt(q)) * Integer.parseInt(d);
-		TotalAmount += (Integer.parseInt(quantity) % Integer.parseInt(q)) * unitPrice.get(item);
-
-		return TotalAmount;
+	private static void calTotal(List<String> items) {
+		for (String k : promotions.keySet()) {
+			String q = promotions.get(k).split("-")[0];
+			String d = promotions.get(k).split("-")[2];
+			if (k.length() == 1) {
+				int count = Collections.frequency(items, k);
+				TotalAmount += (count / Integer.parseInt(q)) * Integer.parseInt(d);
+				TotalAmount += (count % Integer.parseInt(q)) * unitPrice.get(k);
+			} else {
+				char[] arr = k.toCharArray();
+				int c1 = Collections.frequency(items, String.valueOf(arr[0]));
+				int c2 = Collections.frequency(items, String.valueOf(arr[1]));
+				if (c1 >= c2) {
+					TotalAmount += (c1 - (c1 - c2)) * Integer.parseInt(d);
+					TotalAmount += (c1 - c2) * unitPrice.get(String.valueOf(arr[0]));
+				} else {
+					TotalAmount += (c2 - (c2 - c1)) * Integer.parseInt(d);
+					TotalAmount += (c2 - c1) * unitPrice.get(String.valueOf(arr[1]));
+				}
+			}
+		}
 	}
 
 }
--- test/promotion/engine/PromotionEngineTest.java
+++ test/promotion/engine/PromotionEngineTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -37,6 +39,26 @@ public class PromotionEngineTest {
 
 	@Test
 	public void test3() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "1-C"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 20);
+	}
+
+	@Test
+	public void test4() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "1-D"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 15);
+	}
+
+	@Test
+	public void test5() {
 		String simulatedUserInput = "2" + System.getProperty("line.separator") + "1-A"
 				+ System.getProperty("line.separator") + "1-B" + System.getProperty("line.separator");
 		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
@@ -46,7 +68,29 @@ public class PromotionEngineTest {
 	}
 
 	@Test
-	public void test4() {
+	public void test6() {
+		String simulatedUserInput = "3" + System.getProperty("line.separator") + "1-A"
+				+ System.getProperty("line.separator") + "1-B" + System.getProperty("line.separator") + "1-C"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 100);
+	}
+
+	@Test
+	public void test7() {
+		String simulatedUserInput = "3" + System.getProperty("line.separator") + "1-A"
+				+ System.getProperty("line.separator") + "1-B" + System.getProperty("line.separator") + "1-D"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 95);
+	}
+
+	@Test
+	public void test8() {
 		String simulatedUserInput = "1" + System.getProperty("line.separator") + "3-A"
 				+ System.getProperty("line.separator");
 		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
@@ -56,7 +100,7 @@ public class PromotionEngineTest {
 	}
 
 	@Test
-	public void test5() {
+	public void test9() {
 		String simulatedUserInput = "1" + System.getProperty("line.separator") + "2-B"
 				+ System.getProperty("line.separator");
 		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
@@ -66,7 +110,7 @@ public class PromotionEngineTest {
 	}
 
 	@Test
-	public void test6() {
+	public void test10() {
 		String simulatedUserInput = "1" + System.getProperty("line.separator") + "4-A"
 				+ System.getProperty("line.separator");
 		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
@@ -76,13 +120,55 @@ public class PromotionEngineTest {
 	}
 
 	@Test
-	public void test7() {
+	public void test11() {
 		String simulatedUserInput = "1" + System.getProperty("line.separator") + "3-B"
 				+ System.getProperty("line.separator");
 		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
 		System.setIn(in);
 		PromotionEngine.main(null);
 		assertEquals(PromotionEngine.TotalAmount, 75);
+	}
+
+	@Test
+	public void test12() {
+		String simulatedUserInput = "2" + System.getProperty("line.separator") + "1-C"
+				+ System.getProperty("line.separator") + "1-D" + System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 15);
+	}
+
+	@Test
+	public void test13() {
+		String simulatedUserInput = "2" + System.getProperty("line.separator") + "2-C"
+				+ System.getProperty("line.separator") + "1-D" + System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 35);
+	}
+
+	@Test
+	public void test14() {
+		String simulatedUserInput = "4" + System.getProperty("line.separator") + "1-A"
+				+ System.getProperty("line.separator") + "1-B" + System.getProperty("line.separator") + "1-C"
+				+ System.getProperty("line.separator") + "1-D" + System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 95);
+	}
+
+	@Test
+	public void test15() {
+		String simulatedUserInput = "4" + System.getProperty("line.separator") + "4-A"
+				+ System.getProperty("line.separator") + "5-B" + System.getProperty("line.separator") + "3-C"
+				+ System.getProperty("line.separator") + "2-D" + System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 350);
 	}
 
 }
