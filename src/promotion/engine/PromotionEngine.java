cd9f96fc8568a4069f3d715b020770c3e9236951
Kishan_KK <kishankalburgi22@gmail.com>
2020-10-04 12:55:07 +0530

    Adapted code to handle A & B parts as input

--- src/promotion/engine/PromotionEngine.java
+++ src/promotion/engine/PromotionEngine.java
@@ -1,10 +1,52 @@
 package promotion.engine;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class PromotionEngine {
 	public static int TotalAmount = 0;
+	private static Map<String, Integer> unitPrice = new HashMap<>();
+	private static Map<String, String> promotions = new HashMap<>();
+
+	private static void init() {
+		// Unit price SKU
+		unitPrice.put("A", 50);
+		unitPrice.put("B", 30);
+		unitPrice.put("C", 20);
+		unitPrice.put("D", 15);
+
+		// Promotions
+		promotions.put("A", "3-A-130");
+		promotions.put("B", "2-B-45");
+		promotions.put("CD", "1-CD-15");
+
+	}
 
 	public static void main(String[] args) {
+		init();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of SKUs");
 
+		int total = sc.nextInt();
+
+		System.out.println("Enter the " + total + " input items in -> [item - quantity] format E.g 1-A");
+		for (int i = 0; i < total; i++) {
+			calTotal(sc.next());
+		}
+		System.out.println("The Total Amount after discount is - " + TotalAmount);
+	}
+
+	public static int calTotal(String input) {
+		String quantity = input.split("-")[0];
+		String item = input.split("-")[1];
+
+		String q = promotions.get(item).split("-")[0];
+		String d = promotions.get(item).split("-")[2];
+		TotalAmount += (Integer.parseInt(quantity) / Integer.parseInt(q)) * Integer.parseInt(d);
+		TotalAmount += (Integer.parseInt(quantity) % Integer.parseInt(q)) * unitPrice.get(item);
+
+		return TotalAmount;
 	}
 
 }
--- test/promotion/engine/PromotionEngineTest.java
+++ test/promotion/engine/PromotionEngineTest.java
@@ -2,14 +2,87 @@ package promotion.engine;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public class PromotionEngineTest {
 
+	@AfterEach
+	public void reset() {
+		PromotionEngine.TotalAmount = 0;
+	}
+
 	@Test
 	public void test1() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "1-A"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
 		PromotionEngine.main(null);
-		assertEquals(PromotionEngine.TotalAmount, 0);
+		assertEquals(PromotionEngine.TotalAmount, 50);
+	}
+
+	@Test
+	public void test2() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "1-B"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 30);
+	}
+
+	@Test
+	public void test3() {
+		String simulatedUserInput = "2" + System.getProperty("line.separator") + "1-A"
+				+ System.getProperty("line.separator") + "1-B" + System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 80);
+	}
+
+	@Test
+	public void test4() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "3-A"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 130);
+	}
+
+	@Test
+	public void test5() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "2-B"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 45);
+	}
+
+	@Test
+	public void test6() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "4-A"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 180);
+	}
+
+	@Test
+	public void test7() {
+		String simulatedUserInput = "1" + System.getProperty("line.separator") + "3-B"
+				+ System.getProperty("line.separator");
+		InputStream in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+		System.setIn(in);
+		PromotionEngine.main(null);
+		assertEquals(PromotionEngine.TotalAmount, 75);
 	}
 
 }
